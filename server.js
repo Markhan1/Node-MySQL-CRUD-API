d7d48c4edef990b16cfe4c9ef942f312a5ee344a
Markhan1 <markhan1@protonmail.com>
2022-10-11 16:43:25 +0800

    modified by Markhan1

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config({ path: './.env' });
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// create express app
+const app = express();
+
+// Setup server port
+const port = process.env.PORT;
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// define a root route
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+
+// listen for requests
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
